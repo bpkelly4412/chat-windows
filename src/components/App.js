e1825b6cd09198f0a36e7bc75438ebf2b760b7de
Brendan Kelly <bpkelly@umass.edu>
2017-04-03 21:01:37 -0400

    message stylings

--- src/components/App.js
+++ src/components/App.js
@@ -13,9 +13,27 @@ class App extends Component {
           <div className="row">
             <div className="col-md-5 chat-box">
               <div className="row">
-                <div className="chat-header col-md-12">
+                <div className="col-md-12 chat-header">
                   <p><span className="glyphicon glyphicon-comment"></span>Username</p>
                 </div>
+              </div>
+              <div className="row">
+                <div className="message received"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus fermentum ante. Suspendisse nec libero vehicula, ultricies ligula a, luctus mi. Morbi dictum lobortis ipsum. In aliquet mauris libero, at tempor libero suscipit nec. Nam neque lorem, placerat bibendum justo nec, pretium lacinia mauris. Quisque porta tincidunt libero nec rutrum.</p></div>
+              </div>
+              <div className="row">
+                <div className="message sent pull-right"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus fermentum ante. Suspendisse nec libero vehicula, ultricies ligula a, luctus mi. Morbi dictum lobortis ipsum. In aliquet mauris libero, at tempor libero suscipit nec. Nam neque lorem, placerat bibendum justo nec, pretium lacinia mauris. Quisque porta tincidunt libero nec rutrum.</p></div>
+              </div>
+              <div className="row">
+                <div className="message sent pull-right"><p>Lorem ipsum</p></div>
+              </div>
+              <div className="row">
+                <div className="message sent pull-right"><p>Lorem ipsum</p></div>
+              </div>
+              <div className="row">
+                <div className="message received "><p>Lorem ipsum</p></div>
+              </div>
+              <div className="row">
+                <div className="message received"><p>Lorem ipsum</p></div>
               </div>
             </div>
           </div>
